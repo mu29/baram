d74cef4a2a96fb0a1491bd3c05864b010a0feb4e
InJung Chung <mu29gl@gmail.com>
2015-05-06 14:38:37 +0900

    Update main

--- PCEngine/PCEngine/main.cpp
+++ PCEngine/PCEngine/main.cpp
@@ -6,55 +6,27 @@
 //  Copyright (c) 2015년 작은별. All rights reserved.
 //
 
+#include <iostream>
 #include <SDL2/SDL.h>
-#include <stdio.h>
 
-// 스크린 크기
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+#include "Screen.h"
+using namespace baram;
+
 
 int main( int argc, char* args[] )
 {
-    // 윈도우
-    SDL_Window* window = NULL;
+    Screen* screen = new Screen();
     
-    // SDL 서피스
-    SDL_Surface* screenSurface = NULL;
-    
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-    }
-    else
-    {
-        //Create window
-        window = SDL_CreateWindow( "바람", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
-        {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+    bool loop = true;
+    while (loop) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT)
+                loop = false;
         }
-        else
-        {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
-            
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
-            
-            //Wait two seconds
-            SDL_Delay( 2000 );
-        }
+        screen->draw();
+        SDL_Delay( 16 );
     }
-    
-    //Destroy window
-    SDL_DestroyWindow( window );
-    
-    //Quit SDL subsystems
-    SDL_Quit();
     
     return 0;
 }
